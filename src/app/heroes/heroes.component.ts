2aafddfc626459d6d1d487b9bdf400d680c59d4d
Sara Laffin <sarala@umich.edu>
2020-07-29 00:30:43 -0400

    add dashboard and route to hero detail page

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +9,8 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
-  onSelect(hero:Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected ${hero.name} hero id=${hero.id}`)
-  }
-
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
   getHeroes(): void {
     this.heroService.getHeroes()
