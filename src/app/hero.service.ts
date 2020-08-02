21dd7c454c8099f91c910d77fb214731651b268a
Sara Laffin <sarala@umich.edu>
2020-08-02 16:45:50 -0400

    add delete feature

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -74,8 +74,17 @@ export class HeroService {
 
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap((newHero: Hero)=> this.log(`added hero w/id=${newHero.id}`)),
+      tap((newHero: Hero) => this.log(`added hero w/id=${newHero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
+    )
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero ==='number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
     )
   }
 }
